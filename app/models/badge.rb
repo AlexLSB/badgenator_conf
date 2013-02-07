932484778e5c62bc0e045a13f19c950bb7d641e2
AlexLSB <your_email@whatever.com>
2013-02-07 17:51:34 +0600

    Validations ok

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,4 +1,13 @@
 class Badge < ActiveRecord::Base
   belongs_to :badge_set
+  validates :badge_set_id, :presence => true
+  validates :name, :presence => true
+  validates :company, :presence => true
+  validates :profession, :length => {:minimum => 3, :maximum => 30}
+  validates :surname, :length => {:minimum => 2, :maximum => 30}
+  validates :company, :length => {:minimum => 3, :maximum => 30}
+  validates :name, :length => {:minimum => 2, :maximum => 30}
+
   attr_accessible :badge_set_id, :company, :created_at, :name, :profession, :surname, :updated_at
+
 end
--- app/models/badge_set.rb
+++ app/models/badge_set.rb
@@ -1,6 +1,9 @@
 class BadgeSet < ActiveRecord::Base
-   has_many :badges
+   validates  :name, :presence => true, :length => { :minimum => 2, :maximum => 50}
+
+   has_many :badges	
    attr_accessible :name 
+
    attr_accessible :image
    attr_accessor :name
    attr_accessor :image
